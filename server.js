b9b42010190dc849e1941388f946cd9c45b61be8
anhvungoc21 <anhvungoc.21@gmail.com>
2022-05-20 06:25:28 -0500

    Fixed server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname)));
 
 //where we set our routes
-app.get("../../", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
